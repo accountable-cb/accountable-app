5ac9133736ae7923abdb81168e88d07809bf24ee
Ian Christie <iankchristie@gmail.com>
2024-05-20 15:55:55 -0600

    Submit UI.

--- app/components/FoodCounter.tsx
+++ app/components/FoodCounter.tsx
@@ -1,30 +1,34 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import Cow from "../../assets/cow.svg";
 
 interface FoodCounterProps {
   name: string;
   count: number;
   increment: () => void;
-  decrement: () => void;
 }
 
 export const FoodCounter = (props: FoodCounterProps) => {
   return (
-    <View style={styles.container}>
-      <Text>{props.name}</Text>
-      <TouchableOpacity onPress={props.decrement}>
-        <Text>-</Text>
-      </TouchableOpacity>
-      <Text>{props.count}</Text>
-      <TouchableOpacity onPress={props.increment}>
-        <Text>+</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={props.increment}>
+      <Cow height="100" width="100"></Cow>
+      <Text style={{ fontWeight: "bold" }}>
+        {props.name}: {props.count}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
+    width: "45%",
+    aspectRatio: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 10,
+    padding: 10,
   },
 });
--- app/screens/Logger.tsx
+++ app/screens/Logger.tsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useState } from "react";
 import { FoodCounter } from "../components/FoodCounter";
 import { useAuth } from "../contexts/AuthContext";
 import { getDateFromDayNumber, getFormattedDate } from "../utils/date";
 import { FoodLog } from "../types/definitions";
 import { logFood } from "../api/firebase";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Logger = ({ route, navigation }) => {
   const { log } = route.params;
@@ -23,16 +31,13 @@ const Logger = ({ route, navigation }) => {
     };
   };
 
-  const decrement = (attr: string) => {
-    return () => {
-      setWorkingLog((prevLog) => {
-        const newLog = {
-          ...prevLog,
-          [attr]: Math.max((prevLog[attr] || 0) - 1, 0),
-        };
-        return newLog;
-      });
-    };
+  const clearAll = () => {
+    setWorkingLog({
+      id: workingLog.id,
+      beef: 0,
+      chicken: 0,
+      plant: 0,
+    });
   };
 
   const logFoodLog = async () => {
@@ -41,33 +46,73 @@ const Logger = ({ route, navigation }) => {
   };
 
   return (
-    <View>
-      <Text>{getFormattedDate(getDateFromDayNumber(log.id))}</Text>
-      <FoodCounter
-        name="Beef"
-        count={workingLog.beef}
-        decrement={decrement("beef")}
-        increment={increment("beef")}
-      />
-      <FoodCounter
-        name="Chicken"
-        count={workingLog.chicken}
-        decrement={decrement("chicken")}
-        increment={increment("chicken")}
-      />
-      <FoodCounter
-        name="Plant"
-        count={workingLog.plant}
-        decrement={decrement("plant")}
-        increment={increment("plant")}
-      />
-      <TouchableOpacity onPress={logFoodLog}>
-        <Text>Submit</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Which food did you eat?</Text>
+        <TouchableOpacity onPress={clearAll}>
+          <Text style={styles.clearAllText}>Clear All</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <FoodCounter
+          name="Beef"
+          count={workingLog.beef}
+          increment={increment("beef")}
+        />
+        <FoodCounter
+          name="Chicken"
+          count={workingLog.chicken}
+          increment={increment("chicken")}
+        />
+        <FoodCounter
+          name="Plant"
+          count={workingLog.plant}
+          increment={increment("plant")}
+        />
+      </ScrollView>
+      <TouchableOpacity style={styles.bottomButton} onPress={logFoodLog}>
+        <Text style={styles.bottomButtonText}>Submit</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default Logger;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "white",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  clearAllText: {
+    fontSize: 16,
+    color: "green",
+  },
+  scrollContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  bottomButton: {
+    backgroundColor: "#4CAF50", // Green background
+    padding: 20,
+    borderRadius: 30,
+    marginHorizontal: 24,
+    alignItems: "center",
+  },
+  bottomButtonText: {
+    fontSize: 18,
+    color: "white",
+  },
+});
